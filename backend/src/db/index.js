b73a69a84e5a83ecf703ecfd51512eb6734d4634
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-08-16 16:34:48 +0530

    add ApiResponse and ApiError class

---
+++ backend/src/db/index.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async()=>{
+    try {
+        const db = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        db.connection.on("connected",()=>{
+            console.log("MongoDB connected with instance: ",db);
+        })
+        db.connection.on("error",(error)=>{
+            console.log("MongoDB connection error", error);
+            process.exit(1);
+        })
+        db.connection.on("disconnected",()=>{
+            console.log("MongoDB disconnected");
+        })
+        console.log("MongoDB connected", db.connection.host);
+    } catch (error) {
+        console.log("MongoDB connection failed",error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import { app } from "./app.js";
+dotenv.config({
+    path: "./.env"
+})
+const app = express();
+
+connectDB()
+.then(
+    ()=>{
+        app.listen(process.env.PORT || 8000, ()=>{
+            console.log(`Server is running at port : ${process.env.PORT}`);
+        })
+    }
+)
+.catch(
+    (err) => {
+        console.log("MongoDB connection error", err);
+    }
+)
