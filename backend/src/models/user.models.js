65684516111e4d4167b1e42919e22b1daabad6c2
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-08-16 12:08:39 +0530

    define project structure

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,4 +1,3 @@
-import { profile } from "console";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -45,7 +44,8 @@ const userSchema = new mongoose.Schema({
         }
     ] ,
     EngineeringDomain:{
-        type:String
+        type:String,
+        enum:["CSE","ISE", "ECE", "MECH", "CST", "EEE"]
     },
     University:{
         type:String
