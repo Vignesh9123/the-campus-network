fbcb6678cb169dbb4280feaa7fc10ebc5c25efc6
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-08-17 10:00:31 +0530

    add post and university model

---
+++ backend/src/models/post.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+  modifiedOn: {
+    type: Date,
+    default: Date.now,
+  },
+  tags: [
+    {
+      type: String,
+    },
+  ],
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  public: {
+    type: Boolean,
+    default: true,
+  },
+  onlyFollowers: {
+    type: Boolean,
+    default: false,
+  },
+},{
+
+    timestamps: true
+
+});
+
+export const Post = mongoose.model("Post", postSchema);
