29bd3500154297b275fedff6b4ea8b639ed143c7
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-08-22 18:27:43 +0530

    fix cors bugs

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,8 @@ import cookieParser from 'cookie-parser';
 const app = express()
 app.use(cookieParser())
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
+    origin: process.env.CORS_ORIGIN,
+  credentials: true,
 }))
 
 app.use(express.json({limit:"16kb"}))
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -135,6 +135,7 @@ const loginUser = asyncHandler(async (req, res) =>{
 
   const options = {
       httpOnly: true,
+      // secure: true
   }
 
   return res
