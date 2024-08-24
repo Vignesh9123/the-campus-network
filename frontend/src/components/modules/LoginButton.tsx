7431c96a8734e66fd3f9c7ab1d23b683abf6aef7
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-08-24 17:59:08 +0530

    add navbar

---
+++ frontend/src/components/modules/LoginButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+const LoginButton = () => {
+  return (
+    <div>
+        <Button>Login/Register</Button>
+    </div>
+  )
+}
+
+export default LoginButton
---
+++ frontend/src/components/modules/SearchBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Search } from 'lucide-react'
+import { Input } from '../ui/input'
+const SearchBar = () => {
+  return (
+    <div>
+      <div className="relative ml-auto flex-1 md:grow-0">
+            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              type="search"
+              placeholder="Search..."
+              className="w-full rounded-lg bg-background pl-8 md:w-[350px] lg:w-[200px] xl:w-[350px]"
+            />
+          </div>
+    </div>
+  )
+}
+
+export default SearchBar
