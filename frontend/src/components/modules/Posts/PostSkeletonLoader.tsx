1233db74a923aafa24e8a02f1a5cf46c491eb791
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-09-19 10:57:47 +0530

    add explore posts skeletonLoader

---
+++ frontend/src/components/modules/Posts/PostSkeletonLoader.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+function PostSkeletonLoader() {
+  return (
+    <div>
+      <div className="postcard m-10 mt-3 animate-pulse">
+  <div className="flex header items-center gap-2">
+    <div className="flex gap-2 items-center">
+      <div>
+        {/* Skeleton for profile picture */}
+        <div className="w-10 h-10 rounded-full bg-muted"></div>
+      </div>
+      <div className="flex-col">
+        {/* Skeleton for username */}
+        <div className="w-24 h-4 bg-muted rounded-md mb-1"></div>
+        {/* Skeleton for post creation time */}
+        <div className="w-16 h-4 bg-muted rounded-md"></div>
+      </div>
+    </div>
+    <div className="ml-auto pr-10">
+      {/* Skeleton for follow button */}
+      <div className="w-16 h-6 bg-muted rounded-md"></div>
+    </div>
+  </div>
+
+  <div className="w-3/4 h-[2px] mx-auto m-4 bg-muted"></div>
+
+  {/* Skeleton for post title */}
+  <div className="w-1/2 h-6 bg-muted rounded-md m-1"></div>
+
+  {/* Skeleton for post content */}
+  <div className="w-full h-20 bg-muted rounded-md mt-2 mb-4"></div>
+
+  <div className="w-full h-[2px] bg-muted mb-2"></div>
+
+  <div className="flex items-center justify-around gap-2 m-3">
+    {/* Skeleton for Like */}
+    <div className="flex p-2 items-center gap-3">
+      <div className="w-5 h-5 bg-muted rounded-md"></div>
+      <div className="w-8 h-4 bg-muted rounded-md"></div>
+    </div>
+
+    {/* Skeleton for Comment */}
+    <div className="flex p-2 items-center gap-3">
+      <div className="w-5 h-5 bg-muted rounded-md"></div>
+      <div className="w-8 h-4 bg-muted rounded-md"></div>
+    </div>
+
+    {/* Skeleton for Repost */}
+    <div className="flex p-2 items-center gap-3">
+      <div className="w-5 h-5 bg-muted rounded-md"></div>
+      <div className="w-8 h-4 bg-muted rounded-md"></div>
+    </div>
+  </div>
+
+  <div className="w-full h-[2px] bg-muted"></div>
+</div>
+
+    </div>
+  )
+}
+
+export default PostSkeletonLoader
--- frontend/src/pages/ExplorePosts.tsx
+++ frontend/src/pages/ExplorePosts.tsx
@@ -4,18 +4,22 @@ import FloatingActionButton from '@/components/modules/FloatingActionButton'
 import OthersPostCard from '@/components/modules/Posts/OthersPostCard'
 import { useAuth } from '@/context/AuthContext'
 import { getUserFeed } from '@/api'
+import PostSkeletonLoader from '@/components/modules/Posts/PostSkeletonLoader'
 
 function ExplorePosts() {
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
     const {user} = useAuth();
     useEffect(() => {
         const fetchPosts = async () => {
+          setLoading(true);
           try {
             const data = await getUserFeed();
             setPosts(data.data.data);
           } catch (error) {
             console.error('Error fetching posts:', error);
           }
+          setLoading(false);
         };
     
         fetchPosts();
@@ -29,7 +33,18 @@ function ExplorePosts() {
         </div>
         <div className="w-[50%] border-0 border-r-[1px] h-screen overflow-y-auto scrollbar-hide">
             <div className="text-xl font-bold mx-10 my-5 text-center">Explore Posts</div>
-            {posts.map((post:any) => (
+            {loading && 
+            <>
+            <PostSkeletonLoader/>
+            <PostSkeletonLoader/>
+            <PostSkeletonLoader/>
+            </>
+
+            }
+            
+            {!loading && posts.length === 0 && <div className="text-center mt-10">No posts to show, search or follow new content</div>}
+            {!loading && posts.length > 0 &&
+            posts.map((post:any) => (
                 <OthersPostCard otherUser={post.createdBy} key={post._id} post={post} following={post.isFollowedUser} />
             ))}
 
