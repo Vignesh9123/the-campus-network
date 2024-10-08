409bdd2e53512deb28b4b938141d9202e7d6ca6c
Vignesh D <vignesh.d9123@gmail.com>
2024-10-08 13:29:07 +0530

    initial setup

---
+++ backend/src/socket/index.js
@@ -0,0 +1,54 @@
+import {Socket, Server} from 'socket.io'
+import { AvailableChatEvents, ChatEventEnum } from '../constants.js'
+import {User} from '../models/user.model.js'
+import {ApiError} from '../utils/ApiError.js'
+import cookie from 'cookie'
+import jwt from 'jsonwebtoken'
+
+const mountJoinChatEvent = (socket)=>{
+    socket.on(ChatEventEnum.JOIN_CHAT_EVENT, (chatRoomId)=>{
+        socket.join(chatRoomId)
+        console.log(`User joined chat room ${chatRoomId}`)
+    })
+}
+
+//Mount typing event in future
+//Mount stop typing event in future
+
+const initializeSocketIO = (io)=>{
+    return io.on("connection",async (socket)=>{
+        try {
+           const cookies = cookie.parse(socket.handshake.headers.cookie || "")
+           const token = cookies.accessToken
+
+           if(!token){
+            token = socket.handshake.auth.token
+           }
+           if(!token){
+            throw new ApiError(401, "Token is not passed in socket connection")
+           }
+           const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+           const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+           if(!user)
+                throw new ApiError(401, "Invalid access token")
+            socket.user = user
+            socket.join(user._id.toString())
+            socket.emit(ChatEventEnum.CONNECTED_EVENT)
+            console.log("User connected: UserID", user._id.toString())
+            mountJoinChatEvent(socket)
+
+            socket.on(ChatEventEnum.DISCONNECT_EVENT, ()=>{
+                console.log("User disconnected: UserID", socket.user._id.toString())
+                if(socket.user)
+                    socket.leave(user._id.toString())
+            })
+        } catch (error) {
+            socket.emit("error", error.message)
+        }
+    })
+}
+
+const emitSocketEvent = (req, roomId, event, data)=>{
+    req.app.get("io").in(roomId).emit(event, data)
+}
+export {initializeSocketIO, emitSocketEvent}
