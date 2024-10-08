0d4f8c01faffa5d7082422588bed2db2b8de3052
Vignesh D <vignesh.d9123@gmail.com>
2024-10-08 13:34:03 +0530

    models for chat and message

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+// TODO: Add image and pdf file sharing in the next version
+const chatMessageSchema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+    },
+    chat: {
+      type: Schema.Types.ObjectId,
+      ref: "Chat",
+    },
+  },
+  { timestamps: true }
+);
+
+export const ChatMessage = mongoose.model("ChatMessage", chatMessageSchema);
