e8e3c819abcf6f64296418452bc41c2c9564c207
Vignesh D <vignesh.d9123@gmail.com>
2024-10-27 19:29:57 +0530

    log token for debug

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -4,6 +4,7 @@ import { ApiError } from "../utils/ApiError.js";
 export const verifyJWT = async (req, res, next) => {
   try {
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+    console.log("token", token);
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
